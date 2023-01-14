2d71671ae26e16d5a400808be8583f30f442cd61
Lukas Bach <contact@lukasbach.com>
2023-01-14 22:20:22 +0100

    chore: exit template

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
     "airbnb",
@@ -24,12 +23,19 @@ module.exports = {
     },
     project: './tsconfig.json'
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
   rules: {
     "import/prefer-default-export": "off",
     "no-console": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "no-continue": "off",
+    "no-await-in-loop": "off",
+    "react/function-component-definition": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-nested-ternary": "off",
+    "react/require-default-props": "off",
+    "@typescript-eslint/ban-types": "off",
   },
   ignorePatterns: ["lib", "*.js"],
 };
